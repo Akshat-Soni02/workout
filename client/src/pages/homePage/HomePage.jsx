2c116970ee24676f7cc47e3571335742deb462a9
Akshat Soni <akshatsonibhl99@gmail.com>
2025-01-30 18:34:57 +0530

    abcadf

--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -4,6 +4,8 @@ import CustomInput from "../../components/customInput/CustomInput.jsx";
 import CustomForm from "../../components/customForm/CustomForm.jsx";
 import ShadowCard from "../../components/shadowCard/ShadowCard.jsx";
 import WorkoutCard from "../../components/workoutCard/WorkoutCard.jsx";
+import RecordLogParent from "../../components/recordLog/RecordLogParent.jsx";
+
 
 const temp = ['item1','item2','item3','item4'];
 
@@ -14,6 +16,7 @@ const HomePage = () => {
                 <WorkoutCard title={"Workout plan 1"} exercises = {temp} btnText={"Edit"}>Too much content</WorkoutCard>
                 {/* <CustomInput id={"aaa"} label={"aaa"}/> */}
                 {/* <ShadowCard/> */}
+                <RecordLogParent/>
             </div>
         </>
     );
