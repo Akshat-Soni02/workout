34aa9fa466bf295bb5bd5f19ed01533159992dac
Akshat Soni <akshatsonibhl99@gmail.com>
2025-01-31 12:02:20 +0530

    recordLog

--- client/src/components/recordSet/RecordSet.jsx
+++ client/src/components/recordSet/RecordSet.jsx
@@ -2,70 +2,57 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import CustomInput from "../customInput/CustomInput.jsx";
 import CustomButton from "../button/CustomButton.jsx";
-import { Heading, Title, LightText } from "../customTypo/CustomTypo.jsx";
-
+import { Title } from "../customTypo/CustomTypo.jsx";
 import "./style.css";
 
-const RecordSet = () => {
+const RecordSet = ({ setId, onSubmit }) => {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+    const handleFormSubmit = (data) => {
+        console.log(`Set ${setId} submitted:`, data);
+        onSubmit();  // Notify parent that this set is submitted
+    };
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+    return (
+        <form onSubmit={handleSubmit(handleFormSubmit)} className="recordSetForm">
+            <Title style={{ fontSize: "2rem" }}>Set {setId}</Title>
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="recordSetForm">
-        <Title style = {{fontSize: "2rem"}}>Set 1</Title>
-        <CustomInput
-          name="reps"
-          control={control}
-          rules={{
-            required: "Reps are required",
-          }}
-          label="Total Reps"
-          type="reps"
-          sx={{
-            width: "350px"
-          }}
-        />
+            <CustomInput
+                name="reps"
+                control={control}
+                rules={{ required: "Reps are required" }}
+                label="Total Reps"
+                type="reps"
+                sx={{ width: "350px" }}
+            />
 
+            <CustomInput
+                name="weight"
+                control={control}
+                label="Weight Lifted"
+                type="weight"
+                sx={{ width: "350px" }}
+            />
 
-        <CustomInput
-          name="weight"
-          control={control}
-          label="Weight Lifted"
-          type="weight"
-          sx={{
-            width: "350px"
-          }}
-        />
+            <CustomInput
+                name="duration"
+                control={control}
+                label="Duration"
+                type="duration"
+                sx={{ width: "350px" }}
+            />
 
-        <CustomInput
-          name="duration"
-          control={control}
-          label="Duration"
-          type="duration"
-          sx={{
-            width: "350px"
-          }}
-        />
-
-        {/* <CustomButton
-        type="submit"
-        sx={{
-            width: "250px",
-            fontSize: "1.2rem"
-        }}
-        >
-        Submit
-        </CustomButton> */}
-      </form>
-  );
-}
+            <div className="setSubmit" style={{ display: "flex", justifyContent: "center" }}>
+                <CustomButton type="submit" sx={{ width: "150px", fontSize: "1rem" }}>
+                    Submit
+                </CustomButton>
+            </div>
+        </form>
+    );
+};
 
 export default RecordSet;
